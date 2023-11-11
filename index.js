302e1b28e3125ea8fc3f3cd225e947336742804f
AerynQ <aeryn@xion.global>
2023-11-11 10:48:07 +0200

    etherjs-apis

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
 const express = require('express');
-const { ThirdwebSDK } = require('@thirdweb-dev/sdk');
+const { ethers } = require('ethers');
 require('dotenv').config();
-
 
 const app = express();
 app.use(express.json());
 
-let sdk;
-try {
-  sdk = new ThirdwebSDK('https://polygon.llamarpc.com', {
-    // Use the environment variable here
-    apiKey: process.env.THIRDWEB_SECRET_KEY
-  });
-} catch (error) {
-  console.error('Error initializing Thirdweb SDK:', error);
-}
-
-  
+// Set up Ether.js provider
+const provider = new ethers.providers.JsonRpcProvider('https://polygon.llamarpc.com');
 
 // Define the balance checking endpoint
 app.get('/balance/:address/:tokenAddress', async (req, res) => {
     try {
         const { address, tokenAddress } = req.params;
-        const balance = await sdk.getTokenBalance(tokenAddress, address);
-        res.json({ balance });
+        const tokenContract = new ethers.Contract(tokenAddress, [
+            // Minimal ABI to get ERC20 Token balance and decimals
+            "function balanceOf(address owner) view returns (uint256)",
+            "function decimals() view returns (uint8)"
+        ], provider);
+        const balance = await tokenContract.balanceOf(address);
+        const decimals = await tokenContract.decimals();
+
+        // Convert balance to a human-readable format
+        const formattedBalance = ethers.utils.formatUnits(balance, decimals);
+
+        // Round to 2 decimal places
+        const roundedBalance = Number(formattedBalance).toFixed(2);
+        
+        res.json({ balance: roundedBalance });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
+
+
 // Define the allowance checking endpoint
 app.get('/allowance/:ownerAddress/:spenderAddress/:tokenAddress', async (req, res) => {
     try {
         const { ownerAddress, spenderAddress, tokenAddress } = req.params;
-        const allowance = await sdk.getTokenAllowance(tokenAddress, ownerAddress, spenderAddress);
-        res.json({ allowance });
+        const tokenContract = new ethers.Contract(tokenAddress, [
+            // Minimal ABI to get ERC20 Token allowance and decimals
+            "function allowance(address owner, address spender) view returns (uint256)",
+            "function decimals() view returns (uint8)"
+        ], provider);
+        const allowance = await tokenContract.allowance(ownerAddress, spenderAddress);
+        const decimals = await tokenContract.decimals();
+
+        // Convert allowance to a human-readable format
+        const formattedAllowance = ethers.utils.formatUnits(allowance, decimals);
+
+        // Round to 2 decimal places
+        const roundedAllowance = Number(formattedAllowance).toFixed(2);
+        
+        res.json({ allowance: roundedAllowance });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
